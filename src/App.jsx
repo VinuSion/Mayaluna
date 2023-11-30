aff1f063bdaf7cd368e1d7493b70f2e475233e4b
William G (VinuSion) <revisionthegame@gmail.com>
2023-11-30 16:52:20 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Navbar from "@components/LandingPage/Navbar";
+import Hero from "@components/LandingPage/Hero";
+import AboutCard from "@components/LandingPage/AboutCard";
+import Services from "@components/LandingPage/Services";
+import QuoteCalculator from "./components/LandingPage/QuoteCalculator";
+import FormSection from "./components/LandingPage/FormSection";
+import CarouselSection from "./components/LandingPage/CarouselSection";
+import Footer from "./components/LandingPage/Footer";
+
+const App = () => {
+  return (
+    <div className="relative z-0 bg-background">
+      <div className="bg-hero-background bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <AboutCard />
+      <Services />
+      <div className="bg-primary">
+        <QuoteCalculator />
+      </div>
+      <FormSection />
+      <CarouselSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: "400px",
+        ns: "300px",
+      },
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#ECFFE2",
+        secondary: "#7FBF5F",
+        tertiary: "#306B12",
+        background: "#ffffff",
+        button: "#248e46",
+        header: "#154734",
+        foreground: "#000000",
+        mutedForeground: "#7a7a7a",
+        gradientGreen: "#C1FFA3",
+        yellowCall: "#EDC949",
+      },
+      backgroundImage: {
+        "hero-background": "url('https://mayalunaseguros.com/wp-content/uploads/2021/06/familia2-asegurada-con-mayaluna-opt.jpg')",
+        "section-background": "url('https://mayalunaseguros.com/wp-content/uploads/2021/06/fondo-verde-seguros-mayaluna-1.jpg')"
+      },
+    },
+  },
+  plugins: [],
+};
